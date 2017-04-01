ee83bef29c81f6a0fb9afc67fac8a0e6f500e1eb
Gishaan <gupta_i1@denison.edu>
2017-04-01 16:53:55 -0400

    Adding work.py

--- work.py
+++ work.py
@@ -1,22 +1,26 @@
 import os
 from string import punctuation
 from collections import Counter
-from string import punctuation
-from stop_words import get_stop_words
 from math import log
+import matplotlib.pyplot as pyplot
+from bs4 import BeautifulSoup
 
 
 
-def clean_text(file):
-    fileText = file.split()
-    stop_words = get_stop_words('english')
-    allWords = []
-    for word in fileText:
-        if word not in stop_words:
-            word = word.strip().strip(punctuation).strip().lower()
-            allWords.append(word)   
-    return allWords
 
+def clean_text(text):
+    words = text.split()
+    words = [w.strip().strip(punctuation).strip().lower()
+             for w in words]
+    words = [w for w in words 
+             if w                  # remove empty strings
+             and w != 'p'          # remove stray HTML <p> tags
+             and w != 'gt'         # remove stray HTML entities
+             and w != 'lt'
+             and w != 't'          # remove stray contraction tails
+             and w != 's'          # remove stray possesives...
+             and not any(c.isdigit() for c in w)]  # ...and numbers.
+    return words
 
 def counts(l):
     doc_count = Counter()
@@ -29,55 +33,76 @@ def counts(l):
 #    n_docs = len(l) + len(notreetext)
 #    all_words = set(tree_count) | set(notree_count)
     return tree_count
+def graphForDenison():
+    xvaluesDU = []
+    yvaluesDU = []
+    for f in os.listdir('Denison University'):
+        file = open('Denison University/'+f,"r", encoding="utf-8")
+        x = file.read()
+        x1 = clean_text(x)
+        y = counts(x1)
+        soup = BeautifulSoup( x, 'html.parser' )
+        dataTag = soup.find('date')
+        xvaluesDU.append(dataTag.text[0:4])
+        yvaluesDU.append(y['suicide'])
+        pyplot.plot(xvaluesDU, yvaluesDU)
+        pyplot.show()
 
-def intersectSets(s1, s2):
-    s3 = s1 & s2
-    return s3
+def graphForKenyon():
 
+    xvaluesKC = []
+    yvaluesKC = []
+    for f1 in os.listdir('Kenyon College'):
+        file = open('Denison University/'+f1,"r", encoding="utf-8")
+        x = file.read()
+        x1 = clean_text(x)
+        y = counts(x1)
+        soup = BeautifulSoup( x, 'html.parser' )
+        dataTag = soup.find('date')
+        xvaluesKC.append(dataTag.text[0:4])
+        yvaluesKC.append(y['suicide'])
+    pyplot.plot(xvaluesKC, yvaluesKC)
+    pyplot.show()
+
+def graphForOberlinCollege():
+    
+    xvaluesOWU = []
+    yvaluesOWU = []
+    for f3 in os.listdir('Ohio Wesleyan University'):
+        file = open('Ohio Wesleyan University/'+f3,"r", encoding="utf-8")
+        x = file.read()
+        x1 = clean_text(x)
+        y = counts(x1)
+        soup = BeautifulSoup( x, 'html.parser' )
+        dataTag = soup.find('date')
+        xvaluesOWU.append(dataTag.text[0:4])
+        yvaluesOWU.append(y['suicide'])
+    pyplot.plot(xvaluesOWU, yvaluesOWU)
+    pyplot.show()
+
+def Ohio Wesleyan University():
+    xvaluesOC = []
+    yvaluesOC = []
+    for f2 in os.listdir('Oberlin College'):
+        file = open('Oberlin College/'+f2,"r", encoding="utf-8")
+        x = file.read()
+        x1 = clean_text(x)
+        y = counts(x1)
+        soup = BeautifulSoup( x, 'html.parser' )
+        dataTag = soup.find('date')
+        xvaluesOC.append(dataTag.text[0:4])
+        yvaluesOC.append(y['suicide'])
+    pyplot.scatter(xvaluesOC, yvaluesOC)
+    pyplot.show()    
+    
 def main():
-    #for f in os.listdir('Denison University'):
-        #file = open('Denison University/'+f,"r", encoding="utf-8")
-    file = open("pride.txt", "r")
-    x = file.read()
-    x = clean_text(x)
-    y = counts(x)
-    count = 0 
-    for i in y:
-        count = count + y[i]
     
-    z2 = 0
-    z = y
-    for j in y:
-        z[j] = z[j]/count
-        if z[j]>z2:
-            z2 = z[j]
-    print(y['the'])
-    
-##    print(z2)
-##    for i4 in y:
-##        if y[i4]==z2:
-##            print(i4)
-##
-##
-##    file1 = open("b1.txt","r", encoding="utf-8")
-##    x1 = file1.read()
-##    x1 = clean_text(x1)
-##    y1 = counts(x1)
-##    count1 = 0 
-##    for i1 in y1:
-##        count1 = count1 + y1[i1]
-##    z1 = y1
-##    z21=0
-##    for j1 in y1:
-##        z1[j1] = z1[j1]/count1
-##        if z1[j1]>z21:
-##            z21 = z[j1]
-##    print(z21)
-##    for i5 in y1:
-##        if y[i5]==z21:
-##            print(i5)
-   
-    print(count)
+    graphForDenison()
+    graphForKenyon()
+    graphForOberlinCollege()
+    Ohio Wesleyan University()
 
+       
+ 
 
 main()
